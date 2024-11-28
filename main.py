e8fcddf11c51c8907f07cb567a52b207684bdc20
hoigoysohio <njuli@yahoo.com>
2024-11-28 18:21:05 +0000

    Completed

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ num1 = 68
 num2 = 1
 
 result = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2 
+result4 = num1 / num2 
 
-print("The result:", result)
+print("This is the result:", result)
+print("This is the second result:", result2)
+print("This is the third result:", result3)
+print("This is the fourth result:", result4)
